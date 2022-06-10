90a730f34c2f039d3d4fe1d9b78ec942eb2b232d
Lucas Rego <lucas.sr.rg@gmail.com>
2022-06-10 14:20:16 -0300

    fix(database): :bug: fix bug on table creation script

--- mysql/sql/createtable.sql
+++ mysql/sql/createtable.sql
@@ -1,9 +1,10 @@
 USE nodedb;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';
-SELECT host FROM user WHERE user='root';
-UPDATE user SET host = '%' WHERE user ='root';
-FLUSH privileges;
 CREATE TABLE IF NOT EXISTS people( 
     id MEDIUMINT NOT NULL AUTO_INCREMENT, 
     name CHAR(30) NOT NULL,     
     PRIMARY KEY (id) );
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';
+SELECT host FROM user WHERE user='root';
+UPDATE user SET host = '%' WHERE user ='root';
+FLUSH privileges;
+
